9d70459ecd55420409bc6e6b23851a2867bd37f7
Koray Özdemir <koray-oezdemir>
2023-07-13 16:20:24 +0200

    change

--- r.sh
+++ r.sh
@@ -4,7 +4,7 @@
 sudo apt update
 
 # Installieren Sie die notwendigen Pakete
-sudo apt install -y i3-wm picom rofi kitty vim chromium pulseaudio lxappearance pavucontrol nautilus python3 lightdm curl nodejs npm thunar ranger polybar
+sudo apt install -y i3-wm picom rofi kitty vim chromium pulseaudio lxappearance pavucontrol nautilus python3 lightdm curl nodejs npm thunar ranger polybar fish
 
 # Installiere tldr
 sudo npm install -g tldr
